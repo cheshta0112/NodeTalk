c50be625cac1907fd542448be570ae65002e3981
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-12 14:45:45 +0530

    displaying post and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,23 @@
-module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+const Post = require("../models/post");
+
+module.exports.home = async function (req, res) {
+  try {
+    const posts = await Post.find({})
+      .populate("user")
+      // .populate({
+      //     path: 'comments',
+      //     populate: {
+      //         path: 'user'
+      //     }
+      // })
+      .exec();
+
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: posts,
+    });
+  } catch (err) {
+    console.error("Error fetching posts:", err);
+    return res.render("error", { error: "Error fetching posts" });
+  }
 };
