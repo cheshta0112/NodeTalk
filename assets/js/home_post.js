399ad7cd28cc2a413f18b0f97f160c011132b78c
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-14 15:48:04 +0530

    receiving data of the post via ajax

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -11,13 +11,48 @@
         url: "/post/create",
         data: newPostForm.serialize(),
         success: function (data) {
-          console.log(data);
+          let newPost = newPostDom(data.data.post);
+          $("#posts-list-container>ul").prepend(newPost);
         },
         error: function (error) {
           console.log(error.responseText);
         },
       });
     });
+  };
+
+  // method to create a post in DOM
+  let newPostDom = function (post) {
+    return $(`<li id="post-${post._id}">
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/post/destroy/${post.id}">X</a>
+                        </small>
+                       
+                        ${post.content}
+                        <br>
+                        <small>
+                        ${post.user.name}
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${post._id}" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </li>`);
   };
   // method to create a post in DOM
   createPost();
