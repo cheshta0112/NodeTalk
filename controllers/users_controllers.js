25517f8ed652f60b59ae6980efa1a98a540551df
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-16 17:54:52 +0530

    file upload feature for profile pic"

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,7 +1,9 @@
+const User = require("../models/users");
+const fs = require("fs");
+const path = require("path");
 // module.exports.profile = function (req, res) {
 //   res.end("<h1> users profile </h1>");
 // };
-const User = require("../models/users");
 
 module.exports.profile = async function (req, res) {
   try {
@@ -19,14 +21,35 @@ module.exports.profile = async function (req, res) {
 
 module.exports.update = async function (req, res) {
   try {
-    if (req.user.id == req.params.id) {
-      await User.findByIdAndUpdate(req.params.id, req.body);
-      req.flash("success", "Updated!");
+    // if (req.user.id == req.params.id) {
+    //   await User.findByIdAndUpdate(req.params.id, req.body);
+    //   req.flash("success", "Updated!");
+    //   return res.redirect("back");
+    // } else {
+    //   req.flash("error", "Unauthorized!");
+    //   return res.status(401).send("Unauthorized");
+    // }
+
+    let user = await User.findById(req.params.id);
+    User.uploadedAvatar(req, res, function (err) {
+      if (err) {
+        console.log("*****Multer Error: ", err);
+      }
+
+      user.name = req.body.name;
+      user.email = req.body.email;
+
+      if (req.file) {
+        if (user.avatar) {
+          fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+        }
+
+        // this is saving the path of the uploaded file into the avatar field in the user
+        user.avatar = User.avatarPath + "/" + req.file.filename;
+      }
+      user.save();
       return res.redirect("back");
-    } else {
-      req.flash("error", "Unauthorized!");
-      return res.status(401).send("Unauthorized");
-    }
+    });
   } catch (err) {
     console.error("Error updating user:", err);
     return res.render("error", { error: "Error updating user" });
