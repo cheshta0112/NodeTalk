f2ce519e92c1ac459fa15acfd9f55259589c9b5f
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-16 18:13:58 +0530

    Setting Up The Directory Structure for API

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get("/", homeController.home);
 router.use("/users", require("./users"));
 router.use("/post", require("./post"));
 router.use("/comments", require("./comments"));
+
+router.use("/api", require("./api"));
 //for any further routes,access from here
 //router.use("/routerName",require("./routerfiles"));
 
