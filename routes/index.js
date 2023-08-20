07132f72bdf1acf154e8c4fc6bf88bcb2a8e2fa1
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-20 13:56:56 +0530

    action and routes for likes

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get("/", homeController.home);
 router.use("/users", require("./users"));
 router.use("/post", require("./post"));
 router.use("/comments", require("./comments"));
+router.use("/likes", require("./likes"));
 
 router.use("/api", require("./api"));
 //for any further routes,access from here
