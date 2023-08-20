592871c5dab5d41da6a71e96e5d19ed8cd581b8c
Cheshta Pal <cheshtapal07@gmail.com>
2023-08-21 02:33:41 +0530

    setting up the socket.io

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ const passportGoogle = require("./config/passport-google-oauth2-strategy");
 const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 const customMware = require("./config/middleware");
+
+// setup the chat server to be used with socket.io
+const chatServer = require("http").Server(app);
+const chatSockets = require("./config/chat_sockets").chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("chat server is listening on port 5000");
+
 const Toastify = require("toastify-js");
 
 app.use(express.urlencoded({ extended: true }));
